51eca36faefc7bb54f79812da0bba6f38790e499
alvaro <103962133+alvarooocg@users.noreply.github.com>
2024-10-06 17:00:53 +0200

    -

--- Front-end/front-end/src/App.jsx
+++ Front-end/front-end/src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from 'react'
-
 import './App.css'
 
 // Components
 import Header from './components/Header'
 import Hero from './components/Hero'
-import Explore from './components/Explore'
 import Container from './components/Container'
 import Footer from './components/Footer'
 
